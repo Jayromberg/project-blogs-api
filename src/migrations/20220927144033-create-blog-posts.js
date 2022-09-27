56c067894fa4e6f1bddb2dbe5884f0cfbe281d0f
Jayromberg <jayro.dev@gmail.com>
2022-09-27 12:16:10 -0300

    feat: Adicionado a estrutura de ID nas migrations

--- src/migrations/20220927144033-create-blog-posts.js
+++ src/migrations/20220927144033-create-blog-posts.js
@@ -2,20 +2,17 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER 
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('blog_posts');
   }
 };
--- src/migrations/20220927144114-create-posts-categories.js
+++ src/migrations/20220927144114-create-posts-categories.js
@@ -2,20 +2,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('blog_posts', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('blog_posts');
   }
 };
