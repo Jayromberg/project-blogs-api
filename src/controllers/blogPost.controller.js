14bdfff0c0af91dc6cb4d9d9a4cdf4ea25d6bcd3
Jayromberg <jayro.dev@gmail.com>
2022-10-01 20:01:16 -0300

    refactor: Refatorado a blogPost.controller.js para separar os avaliadores

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -1,57 +1,11 @@
-const Joi = require('joi');
 const { blogPostService } = require('../services');
 
-const { validateString } = require('../util/joiValidations');
-
-const bodyValidationToRegisterPost = (req) => {
-  const schema = Joi.object().keys({
-    title: validateString().messages({
-        'string.empty': 'UNDEFINED_FIELD',
-        'any.required': 'UNDEFINED_FIELD',
-      }),
-    content: validateString().messages({
-        'string.empty': 'UNDEFINED_FIELD',
-        'any.required': 'UNDEFINED_FIELD',
-      }),
-    categoryIds: Joi.array().items(Joi.number()).messages({
-      'any.required': 'UNDEFINED_FIELD',
-      'array.base': 'UNDEFINED_FIELD',
-      'number.base': 'UNDEFINED_FIELD',
-    }),
-  });
-
-  return schema.validate(req.body);
-};
-
-const validationToPost = (body) => {
-  const { error } = bodyValidationToRegisterPost(body);
-
-  if (error) {
-    throw new Error(error.message);
-  }
-};
-
-const validateCategories = async (req) => {
-  const { categoryIds } = req.body;
-
-  const promiseGenerate = categoryIds
-    .map((categoryId) => blogPostService.findAllPostCategory(categoryId));
- 
-  if (promiseGenerate.length === 0) return true;
-
-  return Promise.all(promiseGenerate)
-    .then((responses) => responses
-      .some((response) => (response.count === 0)))
-    .catch((error) => error);
-};
-
-const categoriesExist = async (req) => {
-  const response = await validateCategories(req);
-
-  if (response) {
-    throw new Error('CATEGORY_IDS_NOT_FOUND');
-  }
-};
+const {
+  validationToPost,
+  categoriesExist,
+  postExist,
+  userValidatorToUpdate,
+} = require('./validations/blogPost.validations');
 
 const registerPost = async (req, res) => {
   validationToPost(req);
@@ -65,26 +19,11 @@ const getAllPosts = async (_req, res) => {
   res.status(200).json(allPosts);
 };
 
-const postExist = (post) => {
-  if (!post) {
-    throw new Error('POST_DOES_NOT_EXIST');
-  }
-};
-
 const getPostById = async (req, res) => {
   const { id } = req.params;
   const post = await blogPostService.findPostById(id);
   postExist(post);
   res.status(200).json(post);
-};
-
-const userValidatorToUpdate = async (req) => {
-  const { params, user: { id } } = req;
-  const { userId } = await blogPostService.findPostById(params.id);
-
-  if (userId !== id) { 
-    throw new Error('UNAUTHORIZED_USER');
-  }
 };
 
 const updatePost = async (req, res) => {
---
+++ src/controllers/validations/blogPost.validations.js
@@ -0,0 +1,75 @@
+const Joi = require('joi');
+const { blogPostService } = require('../../services');
+const { validateString } = require('../../util/joiValidations');
+
+const bodyValidationToRegisterPost = (req) => {
+  const schema = Joi.object().keys({
+    title: validateString().messages({
+      'string.empty': 'UNDEFINED_FIELD',
+      'any.required': 'UNDEFINED_FIELD',
+    }),
+    content: validateString().messages({
+      'string.empty': 'UNDEFINED_FIELD',
+      'any.required': 'UNDEFINED_FIELD',
+    }),
+    categoryIds: Joi.array().items(Joi.number()).messages({
+      'any.required': 'UNDEFINED_FIELD',
+      'array.base': 'UNDEFINED_FIELD',
+      'number.base': 'UNDEFINED_FIELD',
+    }),
+  });
+
+  return schema.validate(req.body);
+};
+
+const validateCategories = async (req) => {
+  const { categoryIds } = req.body;
+  
+  const promiseGenerate = categoryIds
+  .map((categoryId) => blogPostService.findAllPostCategory(categoryId));
+  
+  if (promiseGenerate.length === 0) return true;
+  
+  return Promise.all(promiseGenerate)
+  .then((responses) => responses
+  .some((response) => (response.count === 0)))
+  .catch((error) => error);
+};
+
+const userValidatorToUpdate = async (req) => {
+  const { params, user: { id } } = req;
+  const { userId } = await blogPostService.findPostById(params.id);
+
+  if (userId !== id) { 
+    throw new Error('UNAUTHORIZED_USER');
+  }
+};
+
+const validationToPost = (body) => {
+  const { error } = bodyValidationToRegisterPost(body);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
+const categoriesExist = async (req) => {
+  const response = await validateCategories(req);
+
+  if (response) {
+    throw new Error('CATEGORY_IDS_NOT_FOUND');
+  }
+};
+
+const postExist = (post) => {
+  if (!post) {
+    throw new Error('POST_DOES_NOT_EXIST');
+  }
+};
+
+module.exports = {
+  validationToPost,
+  categoriesExist,
+  postExist,
+  userValidatorToUpdate,
+};
